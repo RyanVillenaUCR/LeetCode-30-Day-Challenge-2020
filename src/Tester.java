92c2d8098459d8eb8bafe771143b02f49eec0158
RyanVillenaUCR <rvill028@ucr.edu>
2020-04-07 14:30:49 -0700

    Add tester method with example

---
+++ src/Tester.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tester {
+	
+	public static void runTests() {
+		
+		List<Testable> tests = new ArrayList<Testable>();
+		
+		// Select problems to test here
+		tests.add(new Day07CountNumbers());
+		
+		for (Testable testMe : tests) {
+			
+			System.out.println(testMe.runTests() ?
+				testMe.getClass().getSimpleName() + " passed all tests!\n" :
+				testMe.getClass().getSimpleName() + " failed testing...\n");
+		}
+			
+	}
+	
+}
