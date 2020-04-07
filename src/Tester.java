36e778bc6877c6fbefc1907b6d84810fd610afa8
RyanVillenaUCR <rvill028@ucr.edu>
2020-04-07 14:36:15 -0700

    Normalized spacing for printing

--- src/Tester.java
+++ src/Tester.java
@@ -12,6 +12,7 @@ public class Tester {
 		
 		for (Testable testMe : tests) {
 			
+			System.out.println("===" + testMe.getClass().getSimpleName() + "===");
 			System.out.println(testMe.runTests() ?
 				testMe.getClass().getSimpleName() + " passed all tests!\n" :
 				testMe.getClass().getSimpleName() + " failed testing...\n");
