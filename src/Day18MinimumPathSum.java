b4a98b5028080d29834d825cbc1e04cd6b991015
RyanVillenaUCR <rvill028@ucr.edu>
2020-04-22 00:57:24 -0700

    Changed approach to work in the same grid, tested and verified

--- src/Day18MinimumPathSum.java
+++ src/Day18MinimumPathSum.java
@@ -1,107 +1,31 @@
 import java.util.Map;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Set;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 
 public class Day18MinimumPathSum implements Testable {
 
-    private int[][] generateEmptyWeights(int[][] grid) {
-        
-        if (grid.length == 0)
-            return new int[][] {};
-        
-        int[][] newGrid = new int[grid.length][];
-        for (int i = 0; i < grid.length; i++) {
-            newGrid[i] = new int[grid[0].length];
-            Arrays.fill(newGrid[i], Integer.MAX_VALUE);
-        }
-        
-        return newGrid;
-    }
-    
-    private boolean hasNorth(int[][] grid, int row, int col) {
-        
-        return row != 0;
-    }
-    
-    private boolean hasWest(int[][] grid, int row, int col)  {
-        
-        return col != 0;
-    }
-    
-    private int[] stringToCoords(String str) {
-        
-        Scanner sc = new Scanner(str);
-        int[] coords = new int[] { sc.nextInt(), sc.nextInt() };
-        sc.close();
-        return coords;
-    }
-    
-    private String coordsToString(int a, int b) {
-        
-        return Integer.toString(a) + " " + Integer.toString(b);
-    }
-    
-    private Set<String> generateChildren(int[][] grid, int row, int col) {
-        
-        Set<String> newCoords = new HashSet<String>();
-        
-        // Try to go down
-        if (row != grid.length - 1)
-            newCoords.add(coordsToString(row + 1, col));
-        
-        // Try to go right
-        if (col != grid[0].length - 1)
-            newCoords.add(coordsToString(row, col + 1));
-        
-        return newCoords;
-    }
-    
     public int minPathSum(int[][] grid) {
         
-        if (grid.length == 0) return 0;
+        if (grid.length == 0 || grid[0].length == 0)
+            return 0;
         
-        int[][] weights = generateEmptyWeights(grid);
-        
-        Queue<String> frontier = new LinkedList<String>();
-        frontier.add(coordsToString(0,0));
-        
-        while (!frontier.isEmpty()) {
-            
-            int[] pos = stringToCoords(frontier.remove());
-            int row = pos[0];
-            int col = pos[1];
-            
-            if (row == 0 && col == 0)
-                weights[0][0] = grid[0][0];
-            
-            // Process current location
-            else if (!hasNorth(grid, row, col))
-                weights[row][col] =
-                    weights[row][col - 1] + grid[row][col];
-            
-            else if (!hasWest(grid, row, col))
-                weights[row][col] =
-                    weights[row - 1][col] + grid[row][col];
-            
-            // Has both a north and a west
-            else 
-                weights[row][col] =
-                    Math.min(Math.min(
-                            weights[row - 1][col] + grid[row][col],
-                            weights[row][col - 1] + grid[row][col]),
-                            weights[row][col]);
-            
-            // Generate more locations, if any
-            frontier.addAll(generateChildren(grid, row, col));
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                
+                // Top row, excluding 0,0
+                if (i == 0 && j > 0) 
+                    
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                
+                else if (j == 0 && i > 0)
+                    
+                     grid[i][j] = grid[i][j] + grid[i - 1][j];
+                
+                else if (i != 0 && j != 0)
+                     grid[i][j] = grid[i][j] + Math.min(grid[i][j - 1], grid[i - 1][j]);
+            }
         }
-        
-        System.out.println("Weights:\n" + int2DArrToString(weights));
-        return weights[grid.length - 1][grid[0].length - 1];
+        return grid[grid.length - 1][grid[0].length - 1];
     }
     
     
@@ -124,6 +48,31 @@ public class Day18MinimumPathSum implements Testable {
             new int[] { 1,5,1 },
             new int[] { 4,2,1 }
         }, 7);
+        testCases.put(new int[][] {
+            new int[] { 1,3,1,4 },
+            new int[] { 1,2,5,1 },
+            new int[] { 4,2,7,1 }
+        }, 11);
+        testCases.put(new int[][] {
+            new int[] { 69 }
+        }, 69);
+        testCases.put(new int[][]
+            {}
+        , 0);
+        testCases.put(new int[][] {
+            new int[] {7,1,3,5,8,9,9,2,1,9,0,8,3,1,6,6,9,5},
+            new int[] {9,5,9,4,0,4,8,8,9,5,7,3,6,6,6,9,1,6},
+            new int[] {8,2,9,1,3,1,9,7,2,5,3,1,2,4,8,2,8,8},
+            new int[] {6,7,9,8,4,8,3,0,4,0,9,6,6,0,0,5,1,4},
+            new int[] {7,1,3,1,8,8,3,1,2,1,5,0,2,1,9,1,1,4},
+            new int[] {9,5,4,3,5,6,1,3,6,4,9,7,0,8,0,3,9,9},
+            new int[] {1,4,2,5,8,7,7,0,0,7,1,2,1,2,7,7,7,4},
+            new int[] {3,9,7,9,5,8,9,5,6,9,8,8,0,1,4,2,8,2},
+            new int[] {1,5,2,2,2,5,6,3,9,3,1,7,9,6,8,6,8,3},
+            new int[] {5,7,8,3,8,8,3,9,9,8,1,9,2,5,4,7,7,7},
+            new int[] {2,3,2,4,8,5,1,7,2,9,5,2,4,2,9,2,8,7},
+            new int[] {0,1,6,1,1,0,0,6,5,4,3,4,3,7,9,6,1,9}
+        }, 85);
         
         boolean allPass = true;
         
